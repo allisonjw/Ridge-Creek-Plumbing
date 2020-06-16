cf25eb563a6158f8d77f27f0018c83e94b478795
Allison Wagner <allisonjw01@gmail.com>
2020-06-16 13:04:09 -0600

    Adds router to components in app

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.scss';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Nav from '../Nav/Nav';
+import About from '../About/About';
 import Portfolio from '../Portfolio/Portfolio';
 import Services from '../Services/Services';
+import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
 export const App = () => {
   return (
-    <main className="App">
-        <p>
-          Ridge Creek Plumbing
-        </p>
-    </main>
+    <Switch>
+      <Route exact path='/about' render={() => <> <Nav /> <About /> <Footer /> </>}/>
+      <Route path='/portfolio' render={() => <> <Nav /> <Portfolio/> <Footer /> </>}/>
+      <Route path='/services' render={() => <> <Nav /> <Services/> <Footer /> </>}/>
+      <Route path='/contact' render={() => <> <Nav /> <Contact /> <Footer /> </>}/>
+      <Redirect to='/about' />
+    </Switch>
   );
 }
 
