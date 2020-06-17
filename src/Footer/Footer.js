2d0e348cbff63ef8f6a31ad4fa94d54662307c09
Allison Wagner <allisonjw01@gmail.com>
2020-06-17 12:43:12 -0600

    Adds links and classenames to footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.scss';
 
 export const Footer = () => {
     return (
-      <section className="section_footer">
-        <p>
-          footer
-        </p>
-      </section>
+      <footer>
+          <div className="footer_logo-holder">
+          <img className="logo-img" alt="ridge creek plumbing logo" src={require('../images/logo.png')}></img>
+          </div>
+          <div className="footer_links-holder">
+           <Link to="/about">
+             <button className="footer_link"
+             >
+               About
+             </button>
+           </Link>  
+           <Link to="/portfolio">
+             <button className="footer_link"
+             >
+               Portfolio
+             </button>
+           </Link>  
+           <Link to="/services">
+             <button className="footer_link"
+             >
+               Services
+             </button>
+           </Link>  
+           <Link to="/contact">
+             <button className="footer_link"
+             >
+               Contact
+             </button>
+           </Link>  
+          </div>
+          <div className="footer_socialMedia-holder">
+            <p>
+                social media
+            </p>
+          </div>
+      </footer>
     )
 };
 
