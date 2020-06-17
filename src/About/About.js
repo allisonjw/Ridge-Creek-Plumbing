e717b49d649d7774fd2843a0892fbcffe3f50c41
Allison Wagner <allisonjw01@gmail.com>
2020-06-17 15:32:16 -0600

    Adds sections headings and paragraphs to about sections

--- src/About/About.js
+++ src/About/About.js
@@ -7,41 +7,69 @@ export const About = () => {
         <div className="about_banner" role="show slideshow" aria-label="slideshow of images">
           <figure>
             <img 
-              src=""
-              alt="" 
+              className="about_img"
+              src={require('../images/PNW_1.jpeg')}
+              alt="Pacific Northwest" 
             />
+              <figcaption>
+                Image of the puget sound in the Pacific Northwest. 
+              </figcaption>
+            </figure>
+            <figure>
+              <img 
+                className="about_img"
+                src={require('../images/commercial_plumbing.jpeg')}
+                alt="Commercial plumbing" 
+              />
             <figcaption>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                One of our commercial plumbing projects. 
             </figcaption>
             </figure>
             <figure>
               <img 
-                src=""
-                alt="" 
+                className="about_img"
+                src={require('../images/boots.jpeg')}
+                alt="Dirty boots" 
               />
             <figcaption>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. 
-            </figcaption>
-            </figure>
-            <figure>
-              <img 
-                src=""
-                alt="" 
-              />
-            <figcaption>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+                We work rain or shine.
                 </figcaption>
             </figure>
             <figure>
               <img 
-                src=""
-                alt="" 
+                className="about_img"
+                src={require('../images/plumbing_1.jpeg')}
+                alt="Plumbing work in progress" 
               />
             <figcaption>
-                Ut enim ad minima veniam, quis nostrum exercitationem.
+                One of our residential jobs in progress.
             </figcaption>
             </figure>
         </div>
+        <section className="section_about-why">
+            <h2 className="about_h2">Why Ridge Creek</h2>
+            <p className="about_paragraph">
+
+            </p>
+        </section>
+        <section className="section_about-history">
+            <h2 className="about_h2">Our History</h2>
+            <p className="about_paragraph">
+
+            </p>
+        </section>
+        <section className="section_about-mission">
+            <h2 className="about_h2">Mission Statement</h2>
+            <p className="about_paragraph">
+
+            </p>
+        </section>
+        <section className="section_about-testimonials">
+            <h2 className="about_h2">Testimonials</h2>
+            <p className="about_paragraph">
+                
+            </p>
+        </section>
       </main>
     )
 };
