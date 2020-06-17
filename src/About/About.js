a858212bb9c1973c94667e8b8a668d26ac92b1d3
Allison Wagner <allisonjw01@gmail.com>
2020-06-17 14:57:23 -0600

    Adds images figure and figure captions for about slideshow

--- src/About/About.js
+++ src/About/About.js
@@ -3,10 +3,45 @@ import './About.scss';
 
 export const About = () => {
     return (
-      <main className="App">
-        <p>
-          Ridge Creek Plumbing
-        </p>
+      <main>
+        <div className="about_banner" role="show slideshow" aria-label="slideshow of images">
+          <figure>
+            <img 
+              src=""
+              alt="" 
+            />
+            <figcaption>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            </figcaption>
+            </figure>
+            <figure>
+              <img 
+                src=""
+                alt="" 
+              />
+            <figcaption>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. 
+            </figcaption>
+            </figure>
+            <figure>
+              <img 
+                src=""
+                alt="" 
+              />
+            <figcaption>
+                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+                </figcaption>
+            </figure>
+            <figure>
+              <img 
+                src=""
+                alt="" 
+              />
+            <figcaption>
+                Ut enim ad minima veniam, quis nostrum exercitationem.
+            </figcaption>
+            </figure>
+        </div>
       </main>
     )
 };
