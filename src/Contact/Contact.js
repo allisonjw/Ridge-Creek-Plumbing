f9eb84222d4b489af3cf5082797ce8222a9a71ad
Allison Wagner <allisonjw01@gmail.com>
2020-06-19 13:29:54 -0600

    Adds additional JSX and styling to contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Contact.scss';
+import contactImage from '../images/boots_gloves.jpeg';
 
 export const Contact = () => {
     return (
       <section className="section_contact">
-        <p>
-          Contact Us
-        </p>
+        <div className="contact_banner" 
+        aria-label="notification of contact page">
+          <div className="contact_img" style={{ backgroundImage:`linear-gradient(rgba(255, 252, 252, 0.25), rgba(255, 254, 254, 0.15)), url(${contactImage})` }}>
+            <h2 className="contact_banner-h2">
+              Contact Us
+            </h2>
+          </div>
+        </div>
       </section>
     )
 };
