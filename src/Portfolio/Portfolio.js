624033a8e81d397d86ed29c5a25ddd004db0cf25
Allison Wagner <allisonjw01@gmail.com>
2020-06-19 19:21:05 -0600

    Adds banner with image to portfolio

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Portfolio.scss';
+import portfolioImage from '../images/Laurana_Article_Image.jpg';
 
 export const Portfolio = () => {
     return (
       <section className="section_portfolio">
-        <p>
-          Portfolio
-        </p>
+        <div className="portfolio_banner" 
+        aria-label="notification of portfolio page">
+          <div className="portfolio_img" aria-label="" style={{ backgroundImage:`linear-gradient(rgba(255, 252, 252, 0.25), rgba(255, 254, 254, 0.15)), url(${portfolioImage})` }}>
+            <h2 className="portfolio_banner-h2">
+              Portfolio
+            </h2>
+          </div>
+        </div>      
       </section>
     )
 };
