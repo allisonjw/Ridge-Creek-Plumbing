8e75d50661dd50924a3d57c57ee7c0dc37940355
Allison Wagner <allisonjw01@gmail.com>
2020-06-21 17:11:10 -0600

    Adds image for project and animation on hover for display

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -37,7 +37,25 @@ export const Portfolio = () => {
           </li>
         </ul> 
         <section className="portfolio_main">
-          <article className="project"></article>
+          <article className="project">
+            <div 
+              className="reveal-content" 
+              aria-label="holds image that is revealed on hover">
+            <img 
+                 src={require('../images/Laurana_Article_Image.jpg')}
+                 alt="business storefront in Olympia"
+                 className="project_reveal-img">
+               </img>
+            </div>
+            <div 
+              className="reveal-top" 
+              aria-label="name of project that is displayed when not being hovered">
+              <div 
+                className="reveal-content caption" aria-label="title of project that is displayed on page load">
+                  <h4 className="project_reveal-h4">The Laurana</h4>
+              </div>
+           </div>
+          </article>
           <article className="project"></article>
           <article className="project"></article>
           <article className="project"></article>
