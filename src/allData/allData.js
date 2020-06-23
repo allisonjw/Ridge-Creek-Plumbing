388831a1be655abfcdbfb727376e94772d75f9de
Allison Wagner <allisonjw01@gmail.com>
2020-06-23 12:07:31 -0600

    Adds more data objects and images

--- src/allData/allData.js
+++ src/allData/allData.js
@@ -1,59 +1,61 @@
 export const allProjects = [
     {
+        //fake commercial project
         id: 1,
         project_type: 'commercial',
         name: 'Company XYZ',
         location: 'Tacoma',
-        image: '',
+        image: '../images/Point-Ruston.jpg',
     },
     {
         id: 2,
         project_type: 'commercial',
         name: 'The Artisans Group',
         location: 'South Sound Region',
-        image: '',
+        image: '../images/artisans.jpg',
     },
     {
         id: 3,
         project_type: 'commercial',
         name: 'Les Schwab Tire Center',
         location: 'Centralia',
-        image: '',
+        image: '../images/les_schwab.jpg',
     },
     {
         id: 4,
         project_type: 'commercial',
         name: 'BP ARCO',
         location: 'South Sound Region',
-        image: '',
+        image: '../images/ARCO.jpg',
     },
     {
         id: 5,
         project_type: 'commercial',
         name: '7-Eleven',
         location: 'South Sound Region',
-        image: '',
+        image: '../images/7-eleven.png',
     },
     {
+        //fake residence
         id: 6,
         project_type: 'residential',
         name: 'Smith Residence',
         location: 'Tacoma',
-        image: '',
+        image: '../images/residence_project.jpg',
     },
     {
         id: 7,
         project_type: 'residential',
         name: 'Gilbert Residence',
         location: 'Olympia',
-        image: '',
+        image: '../images/gilbert.jpeg',
     },
     {
         id: 8,
         project_type: 'residential',
         name: 'Wagner Residence',
         location: 'Olympia',
-        image: '',
+        image: '../images/wagner.jpg',
     },
     {
         id: 9,
@@ -67,8 +69,13 @@ export const allProjects = [
         project_type: 'hospitality',
         name: 'The Windjammer Hotel',
         location: 'Pacific Beach',
-        image: '',
+        image: '../images/windjammer.jpg',
     },
-
-
+    {
+        id: 10,
+        project_type: 'hospitality',
+        name: 'XYZ Hotel',
+        location: 'Seattle',
+        image: '../images/seattle.jpeg',
+    },
 ];
