bab36c6b6762d9897e2adb624f2098dd73d82721
Allison Wagner <allisonjw01@gmail.com>
2020-06-23 11:24:10 -0600

    Starting to create allData array for display

---
+++ src/allData/allData.js
@@ -0,0 +1,74 @@
+export const allProjects = [
+    {
+        id: 1,
+        project_type: 'commercial',
+        name: 'Company XYZ',
+        location: 'Tacoma',
+        image: '',
+    },
+    {
+        id: 2,
+        project_type: 'commercial',
+        name: 'The Artisans Group',
+        location: 'South Sound Region',
+        image: '',
+    },
+    {
+        id: 3,
+        project_type: 'commercial',
+        name: 'Les Schwab Tire Center',
+        location: 'Centralia',
+        image: '',
+    },
+    {
+        id: 4,
+        project_type: 'commercial',
+        name: 'BP ARCO',
+        location: 'South Sound Region',
+        image: '',
+    },
+    {
+        id: 5,
+        project_type: 'commercial',
+        name: '7-Eleven',
+        location: 'South Sound Region',
+        image: '',
+    },
+    {
+        id: 6,
+        project_type: 'residential',
+        name: 'Smith Residence',
+        location: 'Tacoma',
+        image: '',
+    },
+    {
+        id: 7,
+        project_type: 'residential',
+        name: 'Gilbert Residence',
+        location: 'Olympia',
+        image: '',
+    },
+    {
+        id: 8,
+        project_type: 'residential',
+        name: 'Wagner Residence',
+        location: 'Olympia',
+        image: '',
+    },
+    {
+        id: 9,
+        project_type: 'mixed-use',
+        name: 'The Laurana',
+        location: 'Olympia',
+        image: '../images/Laurana_Article_Image.jpg',
+    },
+    {
+        id: 10,
+        project_type: 'hospitality',
+        name: 'The Windjammer Hotel',
+        location: 'Pacific Beach',
+        image: '',
+    },
+
+
+];
