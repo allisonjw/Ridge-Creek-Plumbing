eaa3531a370ed0c460bc49b0af7a03b3ea8f800c
Allison Wagner <allisonjw01@gmail.com>
2020-07-10 13:26:37 -0600

    Gets nav and footer test passing

---
+++ src/Footer/Footer.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+import { shallow } from 'enzyme';
+
+describe('Footer', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Footer />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
